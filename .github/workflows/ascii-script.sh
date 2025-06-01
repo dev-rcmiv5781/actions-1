7f578b504200949916969d2d609171b755f6493d
dev-rcmiv5781 <147287315+dev-rcmiv5781@users.noreply.github.com>
2025-06-01 09:48:07 +0800

    modified the shell script file

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,4 +1,7 @@
-sudo apt-get install -y
+#!/bin/bash
+sudo apt-get update -y # It's good practice to update package lists first
+sudo apt-get install -y cowsay
+
 cowsay -f dragon "Run for cover, I am a DRAGON...RAWR!!!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
