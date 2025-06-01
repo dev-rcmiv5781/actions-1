6ecfa3a82aa8a7043a768e2a49f7cba6d102118b
dev-rcmiv5781 <147287315+dev-rcmiv5781@users.noreply.github.com>
2025-06-01 09:55:10 +0800

    removed shell script

--- .github/workflows/acii-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y # It's good practice to update package lists first
-sudo apt-get install -y cowsay
-
-cowsay -f dragon "Run for cover, I am a DRAGON...RAWR!!!" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -ltra
